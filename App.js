8d2532267e21a469bdb2d9b35d1478f31fcc4db6
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-09-25 11:54:58 +0530

    RN flexbox

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1  },
+  box1: {
+    flex:1,
+    backgroundColor: 'blue'
   },
+  box2: {
+    flex:12,
+    backgroundColor: 'red'
+  },
+  box3: {
+    flex:5,
+    backgroundColor: 'green'
+  }
 });
+
+export default App;
