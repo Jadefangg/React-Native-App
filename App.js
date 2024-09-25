39d7f466a41fae364760f9d11888362538ef592e
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-09-25 13:16:28 +0530

    NRA final

--- App.js
+++ App.js
@@ -1,18 +1,38 @@
-import { StyleSheet, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Text, Button,TouchableOpacity } from 'react-native';
+import { Alert } from 'react-native';
 
-const [text, setText] = useState('Hello, World!');
-<View>
-<TextInput
-  style={styles.textInput}
-  value={text}
-  onChangeText={setText}
-  placeholder='Type Something Here'
-/>
-<Text>You wrote: {text}</Text>
-</View>
+
+export default function App() {
+  const [text, setText] = useState('Hello, World!');
+
+
+  const alertMyText = () => {
+    Alert.alert(text);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textInput}
+        value={text}
+        onChangeText={setText}
+        placeholder='Type Something Here'
+      />
+      <Text style={styles.textDisplay}>You wrote: {text}</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={alertMyText}
+        >
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
+          </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'darkgray',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
@@ -21,11 +41,22 @@ const styles = StyleSheet.create({
     width: '88%',
     borderWidth: 1,
     height: 50,
-    padding: 10
   },
   textDisplay: {
+    marginTop: 20,
+    fontSize: 18,
+  },
+  button: {
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+    width: '38%',
     height: 50,
-    lineHeight: 50
-  }
-
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: 'salmon',
+    fontSize: 16,
+  },
 });
